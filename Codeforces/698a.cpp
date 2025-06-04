a652b593e056fd959d4f20f046bc41ceff2c9ce4
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-06-04 19:56:05 +0530

    Vacations CF-1400

--- Codeforces/698a.cpp
+++ Codeforces/698a.cpp
@@ -29,7 +29,7 @@ int solve(vi &nums, int i, int prev) {
     if (i >= nums.size())
         return 0;
     
-    int ans = 0;//this is check
+    int ans = 0
     
     if (nums[i] == 0) {
         ans =1+ solve(nums, i + 1, 0);
